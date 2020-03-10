c9c4033c1db172872a53513b2b02d45224f5a9ce
Marek Kowalonek <marek.kowalonek@gmail.com>
2020-03-10 22:24:08 +0100

    init

--- index.js
+++ index.js
@@ -63,4 +63,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-app.listen(5000);
+const port = process.env.PORT || 5000;
+app.listen(port);
